1ffb96cf34197c2a1e62ccadf02c72c8045ee4a0
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-03-09 01:40:43 -0500

    Added basic client getTime and getDate functions

--- Client.java
+++ Client.java
@@ -1,22 +1,80 @@
 import java.net.*;
 import java.io.*;
 
+class InvalidClientCommandException extends Exception {
+    public InvalidClientCommandException(String str) {
+        super(str);
+    }
+}
+
 class Client {
+    private static Socket client;
+    private static BufferedReader in;
+    private static PrintWriter out;
+
     public static void main(String[] args) {
+        System.out.println(getDate());
+        System.out.println(getTime());
+    }
+
+    public static void startConnection(String ip, int port) throws IOException {
+        client = new Socket(ip, port);
+        out = new PrintWriter(client.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    }
+
+    public static String getDate() {
         try {
-            // Create a socket
-            Socket client = new Socket("localhost", 9989);
-            // get input stream
-            BufferedReader in = new BufferedReader(new
-                    InputStreamReader(client.getInputStream()));
-            // read time from server
-            String time = in.readLine();
-            // printing time
-            System.out.println(time);
-            in.close(); // close input stream
-            client.close(); // close socket
+            startConnection("localhost", 9989);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        out.println(ServerCommands.GET_DATE.toString());
+        String resp = null;
+        try {
+            resp = in.readLine();
+            try {
+                verifyValidCommand(resp);
+            } catch (InvalidClientCommandException e) {
+                e.printStackTrace();
+            }
+            in.close();
+            out.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resp;
+    }
+
+    public static String getTime() {
+        try {
+            startConnection("localhost", 9989);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        out.println(ServerCommands.GET_TIME.toString());
+        String resp = null;
+        try {
+            resp = in.readLine();
+            try {
+                verifyValidCommand(resp);
+            } catch (InvalidClientCommandException e) {
+                e.printStackTrace();
+            }
+            in.close();
+            out.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resp;
+    }
+
+    public static void verifyValidCommand(String serverResponse) throws InvalidClientCommandException {
+        if (serverResponse.equals("invalid-command")) {
+            throw new InvalidClientCommandException("Invalid command from client, must be either getTime or getData");
         }
     }
 }
